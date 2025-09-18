ba07c40c4d70cca2f1b7be9e4fe89d8e93b95396
SnowAcer <Rockthebaby@gmail.com>
2025-09-19 00:35:44 +0300

    Реализована смена фамилии сотрудника.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.model.dao.NorthwindDao;
+import org.example.model.dao.NorthwindDaoImplNames_update;
 import org.example.model.dao.NorthwindDaoImplSalary_raise;
 
 
@@ -8,8 +9,11 @@ public class Main {
 
     public static void main(String[] args) {
         NorthwindDao northwindDao = new NorthwindDaoImplSalary_raise();
+        NorthwindDao northwindDao1 = new NorthwindDaoImplNames_update();
 
         //northwindDao.increaseSalary(northwindDao.getEmployees(), northwindDao.getSalary());
+
+        northwindDao.updateNames(northwindDao1.getEmployees());
 
         //northwindDao.updateNames(northwindDao.getEmployees());
     }
--- src/main/java/org/example/model/dao/NorthwindDao.java
+++ src/main/java/org/example/model/dao/NorthwindDao.java
@@ -1,5 +1,8 @@
 package org.example.model.dao;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -26,8 +29,8 @@ public abstract class NorthwindDao {
 
         for (int i = 0; i <= employees.size() - 1; i++) {
             String title = employees.get(i).getTitle();
-            Integer idEmployee = employees.get(1).getId();
-            Integer idSalary = salaries.get(1).getId();
+            Integer idEmployee = employees.get(i).getId();
+            Integer idSalary = salaries.get(i).getId();
 
             // Проверка на нулевые значения и спецсимволы в строке ID.
             if (idEmployee <= 0 || idSalary <= 0 || idSalary.equals('!')) { /// todo прописать спецсимволы в проверку!
@@ -43,22 +46,26 @@ public abstract class NorthwindDao {
             } else if (idEmployee == idSalary) {
                 if ("Sales Representative".equalsIgnoreCase(title)) {
                     BigDecimal salary = salaries.get(i).getValue();
-                    salary = salary.multiply(BigDecimal.valueOf(2));
+                    BigDecimal percent = salary.multiply(BigDecimal.valueOf(0.05));
+                    salary = salary.add(percent);
                     salaries.set(idSalary - 1, new SalariesEntity(idSalary, salary));
                     updateNumberValue++;
                 } else if ("Vice President, Sales".equalsIgnoreCase(title)) {
                     BigDecimal salary = salaries.get(i).getValue();
-                    salary = salary.multiply(BigDecimal.valueOf(2));
+                    BigDecimal percent = salary.multiply(BigDecimal.valueOf(0.15));
+                    salary = salary.add(percent);
                     salaries.set(idSalary - 1, new SalariesEntity(idSalary, salary));
                     updateNumberValue++;
                 } else if ("Sales Manager".equalsIgnoreCase(title)) {
                     BigDecimal salary = salaries.get(i).getValue();
-                    salary = salary.multiply(BigDecimal.valueOf(2));
+                    BigDecimal percent = salary.multiply(BigDecimal.valueOf(0.07));
+                    salary = salary.add(percent);
                     salaries.set(idSalary - 1, new SalariesEntity(idSalary, salary));
                     updateNumberValue++;
                 } else if ("Inside Sales Coordinator".equalsIgnoreCase(title)) {
                     BigDecimal salary = salaries.get(i).getValue();
-                    salary = salary.multiply(BigDecimal.valueOf(2));
+                    BigDecimal percent = salary.multiply(BigDecimal.valueOf(0.08));
+                    salary = salary.add(percent);
                     salaries.set(idSalary - 1, new SalariesEntity(idSalary, salary));
                     updateNumberValue++;
                 }
@@ -73,9 +80,70 @@ public abstract class NorthwindDao {
      * Метод меняет фамилию или имя введенное в консоль
      */
     public void updateNames(List<EmployeeEntity> employees) {
-        /// todo реализовать логику смены значения в ArrayList.
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));) {
+
+            System.out.println("Вы хотите поменять ФИО сотрудника? \n ДА/НЕТ");
+            String answer = reader.readLine();
+
+            if (answer.equalsIgnoreCase("ДА")) {
+                System.out.println(employees);
+
+                System.out.println("Какой пол? \n **Ms/Mr**");
+                String answerTitleCourtesy = reader.readLine();
+                if (answerTitleCourtesy == null || answerTitleCourtesy.isEmpty()) {
+                    System.err.println("Вы ввели пустую строку!");
+                    return;
+                }
+
+                System.out.println("Какое имя у сотрудника?");
+                String answerFirstName = reader.readLine();
+                if (answerFirstName == null || answerFirstName.isEmpty()) {
+                    System.err.println("Вы ввели пустую строку!");
+                    return;
+                }
+
+                System.out.println("Какая фамилия у сотрудника?");
+                String answerLastName = reader.readLine();
+                if (answerLastName == null || answerLastName.isEmpty()) {
+                    System.err.println("Вы ввели пустую строку!");
+                    return;
+                }
+
+                for (int i = 0; i <= employees.size() - 1; i++) {
+                    Integer idEmployee = employees.get(i).getId();
+                    String firstName = employees.get(i).getFirst_name();
+                    String lastName = employees.get(i).getLast_name();
+                    String titleCourtesy = employees.get(i).getTitle_of_courtesy();
+
+                    if (firstName == null || lastName == null || titleCourtesy == null) {
+                        System.err.println("Вы не ввели в поиске значение!");
+                        return;
+                    } else if (firstName == null || lastName == null || titleCourtesy == null) {
+                            System.err.println("Проверьте наличие ФИО и пола в таблице!");
+                            return;
+                        } else if (answerTitleCourtesy.equalsIgnoreCase("Ms")) {
+                            if (answerFirstName.equalsIgnoreCase(firstName) && answerLastName.equalsIgnoreCase(lastName)) {
+                                System.out.println("Введите новую фамилию.");
+                                String newLastName = reader.readLine();
+                                employees.set(idEmployee - 1, new EmployeeEntity(idEmployee, newLastName,
+                                        answerFirstName, answerTitleCourtesy));
+                                System.out.println(employees);
+                            }
+                        } else if (answerTitleCourtesy.equalsIgnoreCase("Mr")) {
+                            /// todo реализация для мужчин
+                        }
+                    }
+                } else {
+                System.err.println("Вы отменили операцию по смене ФИО сотрудника!");
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        }
     }
-}
+
 
 
 
